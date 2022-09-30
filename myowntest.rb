fa4fa6f5b22dc4e195946b091e2b6aa570f2d42d
kimiaf1998 <kajalafshari@yahoo.com>
2022-09-30 16:41:50 -0700

    about keywords changed

--- myowntest.rb
+++ myowntest.rb
@@ -2,5 +2,9 @@ def method_with_var_args(*args)
   args
 end
 
+def method_with_keyword_arguments(one: 1, two: 'two')
+  [one, two]
+end
 
-puts method_with_var_args
+
+puts method_with_keyword_arguments
